0bd9f0872837c94394e4fa2dfe9c3ee59d765576
CrazyGaffer <62564443+CrazyGaffer@users.noreply.github.com>
2024-03-15 07:56:06 +0200

    Initial Commit

---
+++ WeatherPlan/WeatherPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPlanApp.swift
+//  WeatherPlan
+//
+//  Created by Roman Krusman on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
